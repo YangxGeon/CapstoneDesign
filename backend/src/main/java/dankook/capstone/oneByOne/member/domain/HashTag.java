3103250b36331c31952dbfb3ef6021d5959e7578
KKYUHAN <1120kch@naver.com>
2023-05-06 23:34:55 +0900

    test: register 테스트 추가

--- backend/src/main/java/dankook/capstone/oneByOne/member/domain/HashTag.java
+++ backend/src/main/java/dankook/capstone/oneByOne/member/domain/HashTag.java
@@ -1,20 +1,12 @@
 package dankook.capstone.oneByOne.member.domain;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HashTag {
-
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
 
     private HashTagName hashTagName;
 }
