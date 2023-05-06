4ba1a42f7916002e9daff97f56767dfd6f371a5f
KKYUHAN <1120kch@naver.com>
2023-05-06 23:20:32 +0900

    feat: register 구현

--- backend/src/main/java/dankook/capstone/oneByOne/member/domain/HashTag.java
+++ backend/src/main/java/dankook/capstone/oneByOne/member/domain/HashTag.java
@@ -1,6 +1,9 @@
 package dankook.capstone.oneByOne.member.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class HashTag {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private HashTagName hashTagName;
--- backend/src/main/java/dankook/capstone/oneByOne/member/domain/Member.java
+++ backend/src/main/java/dankook/capstone/oneByOne/member/domain/Member.java
@@ -1,24 +1,29 @@
 package dankook.capstone.oneByOne.member.domain;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private String username;
@@ -32,4 +37,17 @@ public class Member {
     @ElementCollection
     @CollectionTable(name = "hashtag", joinColumns = @JoinColumn(name = "hashtag_id"))
     private List<HashTag> hashTags = new ArrayList<>();
+
+    public Member(final String username, final String password, final String name, final LocalDate birthday,
+                  final Gender gender, final String nickname,
+                  final String email, final List<HashTag> hashTags) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.nickname = nickname;
+        this.email = email;
+        this.hashTags = hashTags;
+    }
 }
