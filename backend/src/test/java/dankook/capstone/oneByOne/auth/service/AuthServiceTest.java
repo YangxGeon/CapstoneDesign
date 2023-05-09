62024bdd2d93bf32c1c8cf79fe635e5fdaf7e170
YangxGeon <tmdrjs1117@naver.com>
2023-05-09 18:11:58 +0900

    feature: 연관관계 테스트

--- backend/src/test/java/dankook/capstone/oneByOne/auth/service/AuthServiceTest.java
+++ backend/src/test/java/dankook/capstone/oneByOne/auth/service/AuthServiceTest.java
@@ -3,10 +3,14 @@ package dankook.capstone.oneByOne.auth.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import dankook.capstone.oneByOne.member.domain.Gender;
+import dankook.capstone.oneByOne.member.domain.HashTag;
+import dankook.capstone.oneByOne.member.domain.HashTagName;
 import dankook.capstone.oneByOne.member.domain.Member;
 import dankook.capstone.oneByOne.member.domain.repository.MemberRepository;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +33,7 @@ class AuthServiceTest {
         // given
         final Member member = new Member(
                 "username", "password", "김규한",
-                LocalDate.now(), Gender.M, "nickname", "email@email.com", List.of());
+                LocalDate.now(), Gender.M, "nickname", "email@email.com", List.of(new HashTag(HashTagName.COLLECTION)));
 
         // when
         authService.register(member);
