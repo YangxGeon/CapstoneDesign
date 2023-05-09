86fdf9eb30a7f224e4801e588576782c4c1f8658
YangxGeon <tmdrjs1117@naver.com>
2023-05-09 19:53:29 +0900

    feat: ResponseEntity ok

--- backend/src/main/java/dankook/capstone/oneByOne/auth/controller/AuthController.java
+++ backend/src/main/java/dankook/capstone/oneByOne/auth/controller/AuthController.java
@@ -4,6 +4,7 @@ import dankook.capstone.oneByOne.auth.service.AuthService;
 import dankook.capstone.oneByOne.auth.service.dto.LoginRequest;
 import dankook.capstone.oneByOne.member.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/register")
-    public void register(@RequestBody Member member) {
+    public ResponseEntity<Void> register(@RequestBody Member member) {
         authService.register(member);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/auth/login")
-    public void register(@RequestBody LoginRequest request) {
+    public ResponseEntity<Void> register(@RequestBody LoginRequest request) {
         authService.login(request);
+        return ResponseEntity.ok().build();
     }
 }
